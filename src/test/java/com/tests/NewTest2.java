cbbc85c88a9c875a6ddb64e473fa9db69996742a
Chandini <m.chandini@yahoo.com>
2018-04-27 09:51:42 +0530

    Changes made

--- src/test/java/com/tests/NewTest2.java
+++ src/test/java/com/tests/NewTest2.java
@@ -19,8 +19,15 @@ public class NewTest2 {
 	{
 		System.setProperty("webdriver.chrome.driver", "C:\\Parkavi\\chromedriver.exe");
 		driver=new ChromeDriver();
-		driver.get("http://github.com/");
+		driver.get("http://newtours.demoaut.com/");
 		driver.manage().window().maximize();
+		
+		driver.findElement(By.name("userName")).sendKeys("remi");
+		driver.findElement(By.name("password")).sendKeys("remi");
+		driver.findElement(By.name("login")).click();
+		String title=driver.getTitle();
+		Assert.assertEquals(title, "Find a Flight: Mercury Tours:");
+	
 		
 	}
 
